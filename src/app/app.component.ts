64eb0d0a21f085c19a586544523257b464cb63ca
Mwenda Gray Hat <51136314+HarunHM@users.noreply.github.com>
2020-04-23 15:31:28 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(public Pwa: PwaService) {}
+  installPwa(): void {
+  this.Pwa.promptEvent.prompt();
+}
 }
